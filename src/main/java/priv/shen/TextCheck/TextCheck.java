5b2ffc5ee3061f7dfd35c474a22f7b7bf098c560
zhiqiangShen <1422537078@qq.co>
2018-01-28 20:01:49 +0800

    change the error encode

--- src/main/java/priv/shen/TextCheck/TextCheck.java
+++ src/main/java/priv/shen/TextCheck/TextCheck.java
@@ -10,19 +10,19 @@ import org.languagetool.rules.RuleMatch;
 
 public class TextCheck {
 	public static String check(String text) {
-		//�?要纠错分析的文本�?属语�?
+		//得到要纠错分析的文本所属语言
 		Language language=new BritishEnglish();
 		//得到语言工具对象
 		JLanguageTool jlt=new JLanguageTool(language);
 		
 		StringBuilder sb=new StringBuilder(text);
 		try {
-			//根据文本得到在相应规则下的匹�?
+			//根据文本得到在相应规则下的匹配
 			List<RuleMatch> ruleMatchs=jlt.check(text);
 			System.out.println("postion:suggestion");
 			for (RuleMatch match : ruleMatchs) {
 				System.out.println(match.getFromPos()+"-"+match.getToPos()+":"+match.getMessage());
-				//按照替换建议对文本进行纠�?
+				//按照替换建议对文本进行纠错
 				sb.replace(match.getFromPos(), match.getToPos(), match.getSuggestedReplacements().get(0));
 			}
 		} catch (IOException e) {
